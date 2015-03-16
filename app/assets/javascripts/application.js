ef5488d3cb463606327c3f9e24115b88976d1e4d
Jake Marwil <rjm02006@gmail.com>
2015-03-16 13:35:58 -0600

    add css and js to allow for moving background images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,26 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets 
+//= require bootstrap-sprockets
 //= require turbolinks
+//= require vegas
 //= require_tree .
+
+function backdrop() {
+
+$('body').vegas({
+  overlay: true,
+  transition: 'fade',
+  transitionDuration: 4000,
+  delay: 10000,
+  animation: 'random',
+  animationDuration: 20000,
+  slides: [
+    { src: 'http://www.bighdwallpapers.com/wallpapers/music-beats-abstract-1920x1200.jpg' },
+    { src: 'http://viscoinstrumentalmusicpgcps.weebly.com/uploads/2/4/7/4/24742341/6966130_orig.jpg' },
+    { src: 'http://www.wallpapersgalaxy.com/wp-content/uploads/2013/04/99.jpg' },
+    { src: 'http://www.listofimages.com/wp-content/uploads/2013/07/microphone-music-yellow-vector.jpg' }
+  ]
+});
+
+}
