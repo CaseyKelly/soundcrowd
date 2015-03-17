fbe90f6be6cda60085f43881c0a64b22bbb5d02a
Jake Marwil <rjm02006@gmail.com>
2015-03-17 14:45:25 -0600

    modify index method to sort shows by ticket status, datetime and artist name

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,6 +4,7 @@ class DashboardController < ApplicationController
     @user = User.find_by(oauth_id: session[:user_id])
     @places = DataFetcher.new
     @shows = @places.local_shows
+    @shows.sort_by!{ |a| [a['ticket_status'], a['datetime'], a["artists"].first["name"]] }
     @hash = Gmaps4rails.build_markers(@shows) do |show, marker|
       marker.lat show["venue"]["latitude"]
       marker.lng show["venue"]["longitude"]
