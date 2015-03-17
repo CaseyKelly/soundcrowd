13d4347d208373f3907331d42f3df6ca7dc8b057
CaseyKelly <casey.kelly@colorado.edu>
2015-03-17 11:40:25 -0600

    add show view

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -11,6 +11,16 @@ class DashboardController < ApplicationController
     end
   end
 
+  def showg
+    @user = User.find_by(oauth_id: session[:user_id])
+    @places = DataFetcher.new
+    @shows = @places.local_shows
+    @hash = Gmaps4rails.build_markers(@shows) do |show, marker|
+      marker.lat show["venue"]["latitude"]
+      marker.lng show["venue"]["longitude"]
+      marker.infowindow show["artists"].first["name"]
+    end
+  end
 
 
 end
