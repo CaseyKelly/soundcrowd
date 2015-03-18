f6fa0e5cbf9ab50f5eab278aac436a09a8449ac9
Reyna DeLoge <reyna.deloge@gmail.com>
2015-03-18 15:41:40 -0600

    remove google map

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -20,19 +20,12 @@ class DashboardController < ApplicationController
     @user = User.find_by(oauth_id: session[:user_id])
     @places = DataFetcher.new
     @shows = @places.local_shows
-
   end
 
   def event_venue
     @venues = DataFetcher.new
     @events = @venues.venue_shows(params[:venueid])
     @yelp_info = Yelp.client.search("#{params[:city]}, #{params[:region]}", { term: params[:venue]})
-    @hash = Gmaps4rails.build_markers(@events) do |event, marker|
-      marker.lat event["venue"]["latitude"]
-      marker.lng event["venue"]["longitude"]
-      marker.infowindow event["venue"]["name"]
-    end
-
   end
 
 end
