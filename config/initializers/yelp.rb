39007d3b3050578ccef00fc3549a416fa5f10e52
CaseyKelly <casey.kelly@colorado.edu>
2015-03-18 16:10:26 -0600

    change env to ENV

--- config/initializers/yelp.rb
+++ config/initializers/yelp.rb
@@ -1,6 +1,6 @@
 Yelp.client.configure do |config|
-  config.consumer_key     = env["YELP_KEY"]
-  config.consumer_secret  = env["YELP_SECRET"]
-  config.token            = env["YELP_TOKEN"]
-  config.token_secret     = env["YELP_TOKEN_SECRET"]
+  config.consumer_key     = ENV["YELP_KEY"]
+  config.consumer_secret  = ENV["YELP_SECRET"]
+  config.token            = ENV["YELP_TOKEN"]
+  config.token_secret     = ENV["YELP_TOKEN_SECRET"]
 end
