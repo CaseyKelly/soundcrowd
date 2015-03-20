f7ed09dc57600a44328ca5eea66299d46d2aa2e1
Jake Marwil <rjm02006@gmail.com>
2015-03-20 09:22:54 -0600

    change http to https for images

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -29,11 +29,11 @@ $('body').vegas({
   animation: 'random',
   animationDuration: 20000,
   slides: [
-    { src: 'http://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/5444565ce4b0ef2fa0b33a2b/1414033479399/1-TYH_8865.jpg?format=1000w' },
-    { src: 'http://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/544457d3e4b01431efe8df33/1413765077900/TYH_7186.jpg?format=1000w' },
-    { src: 'http://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/544727b0e4b014352f0c6cf7/1414033755951/TYH_7600+-+Version+2.jpg?format=1000w' },
-    { src: 'http://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/54486481e4b029c53cbce28a/1414034083697/TYH_4254.jpg?format=1000w' },
-    { src: 'http://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/5444569fe4b01431efe8dc2a/1414042991181/5-%281+of+1%29TYH_1450.jpg?format=1000w'}
+    { src: 'https://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/5444565ce4b0ef2fa0b33a2b/1414033479399/1-TYH_8865.jpg?format=1000w' },
+    { src: 'https://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/544457d3e4b01431efe8df33/1413765077900/TYH_7186.jpg?format=1000w' },
+    { src: 'https://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/544727b0e4b014352f0c6cf7/1414033755951/TYH_7600+-+Version+2.jpg?format=1000w' },
+    { src: 'https://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/54486481e4b029c53cbce28a/1414034083697/TYH_4254.jpg?format=1000w' },
+    { src: 'https://static1.squarespace.com/static/54433764e4b09e0c2c24e17b/544347bae4b02d01b1738a29/5444569fe4b01431efe8dc2a/1414042991181/5-%281+of+1%29TYH_1450.jpg?format=1000w'}
   ]
 });
 
